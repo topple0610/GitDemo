43ae1df33fba970e5f2507e744157931edbcba10
topple <topple0610@gmail.com>
2022-09-28 10:49:39 +0800

    "Hello Git"

---
+++ src/main/java/com/topple/git/Demos.java
@@ -0,0 +1,11 @@
+package com.topple.git;
+
+/**
+ * @author topple
+ * @create 2022-09-28-10:48
+ */
+public class Demos {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
